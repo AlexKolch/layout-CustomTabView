14db9529c519548b4fc8a61d42f03c0b9b674927
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-08-21 14:33:06 +0300

    Initial Commit

---
+++ layout#1SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  layout#1SwiftUI
+//
+//  Created by Алексей Колыченков on 21.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ layout#1SwiftUI/layout_1SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  layout_1SwiftUIApp.swift
+//  layout#1SwiftUI
+//
+//  Created by Алексей Колыченков on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct layout_1SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
